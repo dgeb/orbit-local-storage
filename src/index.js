21767c2446536c331e1f831f03f2ac8515d7cef6
Dan Gebhardt <dan@cerebris.com>
2016-09-20 14:44:23 -0400

    Export supportsLocalStorage function.

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import LocalStorageSource from './local-storage-source';
 import LocalStorageBucket from './local-storage-bucket';
+import { supportsLocalStorage } from './lib/local-storage';
 
-export { LocalStorageSource, LocalStorageBucket };
+export { LocalStorageSource, LocalStorageBucket, supportsLocalStorage };
